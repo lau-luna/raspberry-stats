6bf3e82b07f4ebe932ae9ae7627720e737f50c65
Lautaro Luna <lautaroluna906@gmail.com>
2024-11-20 21:58:35 -0300

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+
+
+export default function Home() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <h1>Raspberry Pi 4 de Lau</h1>
+
+        <img src="https://i.pinimg.com/originals/6d/cf/20/6dcf20b7f96dd4108e1f95ee800730f6.gif" alt="" />
+      </main>
+
+
+    </div>
+  );
+}
